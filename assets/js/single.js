d0eb15c753ca309a0343f91eab0bcb1d324a6da7
Anastasia Perez <Stasiperez85@gmail.com>
2022-02-03 22:12:11 -0700

    prep for errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,11 +3,20 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function(){
-
+// grab repo name from url query string
 var queryString = document.location.search;
 var repoName = queryString.split("=")[1];
-getRepoIssues(repoName);
-repoNameEl.textContent = repoName;
+
+if(repoName) {
+  // display repo name on page
+  repoNameEl.textContent = repoName;
+
+  getRepoIssues(repoName);
+}
+else {
+  // if no repo was given, redirect to the homepage
+  document.location.replace("./index.html");
+}
 };
 
 var getRepoIssues = function(repo) {
@@ -28,8 +37,8 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+    // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
