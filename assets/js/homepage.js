4dcc97037db4bdebabe40f6655f9bae0371243ea
Anastasia Perez <Stasiperez85@gmail.com>
2022-02-05 10:44:22 -0700

    add language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -42,6 +43,32 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       alert('Unable to connect to GitHub');
     });
+};
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
+
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -92,3 +119,4 @@ for (var i = 0; i < repos.length; i++) {
 
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
